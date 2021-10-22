afeec480b2f193834987bd314db1b6715342edf5
Socks <patricksutcliffe123@gmail.com>
2021-10-22 16:27:15 -0400

    completed the put requests

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -52,7 +52,6 @@ router.post('/', async (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-        console.log('first')
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -113,8 +112,24 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const proData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!proData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(proData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
